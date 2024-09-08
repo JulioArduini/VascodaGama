55a134c9dbeda67fd1d2bcf1488d6991616e5888
JulioArduini <julioarduini85@gmail.com>
2024-09-08 13:41:28 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 function pesquisar(){
-    let section = document.getElementById("resultados-pesquisa");//Essa linha de código em JavaScript tem como objetivo selecionar um elemento HTML específico dentro de um documento e armazenar uma referência a ele em uma variável JavaScript.//
+let section = document.getElementById("resultados-pesquisa");//Essa linha de código em JavaScript tem como objetivo selecionar um elemento HTML específico dentro de um documento e armazenar uma referência a ele em uma variável JavaScript.//
+let campoPesquisa = document.getElementById("campo-pesquisa").value.toLocaleLowerCase()
 
-let campoPesquisa = document.getElementById("campo-pesquisa").value
+//campoPesquisa = campoPesquisa.toLowerCase()
 
-campoPesquisa = campoPesquisa.toLowerCase()
 
-let tags = "";
 let resultados = "";
 let titulo = "";
-let descricao = "";
+let tags = "";
 
 if(!campoPesquisa){
     section.innerHTML = "<p>Nada foi encontrado. Você precisa digitar o nome de um atleta ou esporte ou algo relacionado ao atleta.</p>"
@@ -18,18 +17,17 @@ if(!campoPesquisa){
 //Para cada dado dentro da lista de dados.
 for (let dado of dados) {
     titulo = dado.titulo.toLowerCase()
-    descricao = dado.descricao.toLocaleLowerCase()
     tags = dado.tags.toLocaleLowerCase()
     //se titulo includes campoPesquisa
-    if (titulo.includes(campoPesquisa)||descricao.includes(campoPesquisa)||tags.includes(campoPesquisa)){
+    if (titulo.includes(campoPesquisa)||tags.includes(campoPesquisa)){
         //cria um novo elemento
         resultados += `
         <div class="item-resultado">
             <h2>
                 <a href="#" target="_blank">${dado.titulo}</a>
             </h2>
-                <p class="descricao-meta">${dado.descricao}
-                </p>
+            <p class="descricao-meta">${dado.descricao}
+            </p.
                 <a href=${dado.link} target= "_black">Mais informações</a>   
         </div>    
 `;
@@ -39,6 +37,12 @@ for (let dado of dados) {
 if(!resultados){
     resultados = "<p>Nada foi encontrado</p>"
 }
-section.innerHTML = resultados;
 
+section.innerHTML = resultados;
 }
+const inputBusca = document.getElementById("campo-pesquisa");
+inputBusca.addEventListener('keypress', (event) =>{
+    if (event.key === 'Enter'){
+        pesquisar();
+    }
+});
